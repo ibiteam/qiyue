8b5e411bff172de4dee764ede2b25429cd93ee70
张立冬 <690191181@qq.com>
2024-11-20 17:16:12 +0800

    fix(Test Case):Supplement TestCase

--- src/Test/QiyueClientTest.php
+++ src/Test/QiyueClientTest.php
@@ -12,23 +12,23 @@ beforeEach(function () {
 });
 
 it('test application auth', function () {
-    // example 1 自动注入调用
+    // example 1 Laravel依赖注入调用
     //$client = new QiyueClient;
 
     // example 2 配置设定 - 明文调用
     // $config = [
-    //     'url' => 'http://example.com/auth_url', //请求地址
-    //     'type' => 'auth', //调用类型 auth授权
+    //     'url' => 'http://example.com/auth_url', //请求地址 必填 
+    //     'type' => 'auth', //调用类型 auth授权 必填 目前实现了 企悦应用授权类[关键字 auth]
     // ];
     // $client = new QiyueClient($config);
 
     //example 3 配置设定 - 加密调用
     $config = [
-        'url' => 'http://example.com/auth_url', //请求地址
-        'app_id' => 0, //企悦应用id
-        'public_key' => '', //企悦应用(app_id)的public_key
-        'private_key' => '', //企悦应用(app_id)的private_key
-        'type' => 'auth', //调用类型 auth授权
+        'url' => 'http://example.com/auth_url', //请求地址 必填 
+        'app_id' => 79, //企悦应用id 非必填 如果应用id为0 则为明文调用
+        'public_key' => '', //企悦应用(app_id)的public_key 非必填 如果app_id非0 此配置必填
+        'private_key' => '', //企悦应用(app_id)的private_key 非必填 如果app_id非0 此配置必填
+        'type' => 'auth', //调用类型 auth授权 必填 目前实现了 企悦应用授权类[关键字 auth]
     ];
     $client = new QiyueClient($config);
 
